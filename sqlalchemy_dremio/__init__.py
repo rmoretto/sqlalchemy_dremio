13519a320fbbc078d047d440393556d99251f4a0
Rodrigo Ce Moretto <rodrigo.ce.moretto@gmail.com>
2022-09-22 19:16:52 -0300

    register the dremio+flight dialect as "dremio" as well
    This is a hack to make this lib work with superset in its current
    version

--- sqlalchemy_dremio/__init__.py
+++ sqlalchemy_dremio/__init__.py
@@ -5,3 +5,5 @@ from sqlalchemy.dialects import registry
 
 # Register the Flight end point
 registry.register("dremio+flight", "sqlalchemy_dremio.flight", "DremioDialect_flight")
+# HACK: Register the flight endpoint as dremio, so superset can see and use the plugin
+registry.register("dremio", "sqlalchemy_dremio.flight", "DremioDialect_flight")
